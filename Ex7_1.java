30468c7ddeac14b54ac3757669f816cef6136d5b
tangxim <1015835607@qq.com>
2017-04-24 23:18:32 +0800

    Create Ex7_1.java

---
+++ Ex7_1.java
@@ -0,0 +1,28 @@
+ package Test7;
+
+public class Ex7_1 {
+	public static void main(String[] args) {
+		String output[]={
+				"The ","quick ","brown ","fox ","jumps ",
+				"over ","the ","lazy ","dog."
+		};
+		int i = 0;
+		while(i < 12){
+			try{
+				System.out.print(output[i++]);
+			}
+			catch(ArrayIndexOutOfBoundsException e)
+			{
+				System.out.println();
+				System.out.println("下标越界异常处理！");
+				System.out.println(e.toString());
+				break;
+			}
+			finally{
+				System.out.println("不管怎样都要执行的语句！");
+			}
+		}
+		System.out.println("haha...");
+	}
+
+}
